70dd06ebfa9bed5fe16360a1ccbd24f6ef15ceed
fermindelgado220618-code <fermindelgado220618@gmail.com>
2026-01-03 10:39:42 -0500

    Update Discord webhook URL

--- trl.js
+++ trl.js
@@ -1,2 +1,2 @@
 // trl.js
-const webhookUrl = 'https://discord.com/api/webhooks/1450231726440710185/hcCgXjuZ8_c_gE8yeVzr0woQL7iLXS6U57m6JNEy4WQcPYzZPWTSgOZrf6dcGwXc7EFl'; // Reemplaza esto con tu Webhook real
+const webhookUrl = 'https://discord.com/api/webhooks/1454910732364812542/eYc89rArTeJ-VGpqHWlS2Xk4BSPghii18O-MURFjfU7Zl34QcP5uansO4EQVaEglBalP'; // Reemplaza esto con tu Webhook real
